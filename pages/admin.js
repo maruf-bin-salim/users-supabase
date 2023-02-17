81afbd8656f10d236b5f40825be631c0ce0d273d
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2023-02-18 01:46:47 +0600

    updated

---
+++ pages/admin.js
@@ -0,0 +1,125 @@
+import { supabase } from '@/lib/client';
+import { Router, useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+import { useSubscription } from 'react-supabase'
+
+
+
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are zero-indexed
+  const year = date.getFullYear().toString();
+  return `${day}-${month}-${year}`;
+}
+
+function User({ index, userID, username, phone, timestamp }) {
+  const router = useRouter();
+
+
+  async function deleteUser(){
+    let { data, error } = await supabase.from('Users').delete().eq('user_id', userID)
+    router.reload();
+  }
+
+
+
+  return (
+    <div className='user'>
+      <div className='user-left'>
+        <div className='index'>
+          {index}
+        </div>
+      </div>
+      <div className='user-main'>
+        <p className='user-name'>{username}</p>
+        <p className='user-phone'>{phone}</p>
+      </div>
+      <div className='user-right'>
+        <p>{formatTimestamp(timestamp)}</p>
+        <img className={'user-delete-button'} src='/delete.png' onClick={deleteUser}></img>
+      </div>
+    </div>
+  )
+}
+
+const Users = () => {
+
+  const router = useRouter();
+
+  const [users, setUsers] = useState([]);
+  let usersComponent;
+
+
+
+  useEffect(() => {
+    const userSubscription = supabase
+      .channel('any_string_you_want')
+      .on(
+        'postgres_changes',
+        {
+          event: 'INSERT',
+          schema: 'public',
+          table: 'Users',
+        },
+        (payload) => {
+          // console.log(payload);
+          router.reload();
+        }
+      )
+      .subscribe();
+
+    return () => {
+      userSubscription.unsubscribe();
+    }
+  }, [])
+
+
+  useEffect(() => {
+
+    async function fetchUsers() {
+      let { data, error } = await supabase.from('Users').select('*');
+      if (data) setUsers(data);
+      // console.log(data);
+    }
+
+    fetchUsers();
+  }, [])
+
+
+
+
+
+
+
+  return (
+    <div className="admin-page">
+      <h1>Admin Page</h1>
+      <div className='admin-prompt'>
+        <div className='admin-upper-container'>
+          <p>customers</p>
+          <p>2/3</p>
+        </div>
+        <p className='prompt'>Customer who booked on this day</p>
+      </div>
+      <div className='users'>
+        {
+
+          users.map((user, index) =>
+            <User
+              key={index}
+              userID={user.user_id}
+              index={index}
+              username={user.username}
+              phone={user.phone}
+              timestamp={user.timestamp}
+            />
+          )
+
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Users;
---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import { supabase } from "@/lib/client";
+import { useState } from "react"
+
+export default function Home() {
+
+  let [username, setUsername] = useState('');
+  let [phone, setPhone] = useState('');
+  let [isLoading, setLoading] = useState(false);
+
+  async function handleSubmit() {
+    setLoading(true);
+    let user = {
+      username: username,
+      phone: phone,
+      timestamp: Date.now(),
+    }
+
+    // save user to database
+    await supabase.from('Users').insert([user]);
+    setLoading(false);
+    setUsername('');
+    setPhone('');
+
+  }
+
+  return (
+    <div className="user-page">
+      {
+        !isLoading &&
+        <>
+          <div className="user-upper-container">
+            <h1>User Page</h1>
+            <p>For reservations, fill in your details</p>
+            <input type="text" placeholder='First Name' value={username} onChange={(e) => { setUsername(e.target.value) }} />
+            <input type="text" placeholder='Number Mob.' value={phone} onChange={(e) => { setPhone(e.target.value) }} />
+          </div>
+          <div className="user-button-container">
+            <button onClick={handleSubmit}>Reservation</button>
+          </div>
+        </>
+      }
+      {
+        isLoading &&
+        <div className="user-upper-container">
+          <h1>Saving your reservation</h1>
+        </div>
+
+      }
+    </div>
+  )
+}
